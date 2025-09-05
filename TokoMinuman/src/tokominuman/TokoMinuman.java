98b7cc20dd967842cb94988274a78d05e35a349f
frdsjhnarfn <firdausjgulla@gmail.com>
2025-09-05 16:22:06 +0700

    Add files via upload

---
+++ TokoMinuman/src/tokominuman/TokoMinuman.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tokominuman;
+
+/**
+ *
+ * @author theus
+ */
+public class TokoMinuman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cover cv = new Cover();
+        cv.setVisible(true);
+        cv.pack();
+        cv.setLocationRelativeTo(null);
+        cv.setDefaultCloseOperation(Cover.EXIT_ON_CLOSE);
+    }
+    
+}
